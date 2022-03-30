1fda5620926c21e0a2da41feda4bb8872a66e30f
Adeniyii <ifedayoadeniyi@gmail.com>
2022-03-30 17:47:10 +0100

    Implement first landing section

---
+++ src/components/MaxWidthWrapper/MaxWidthWrapper.js
@@ -0,0 +1,10 @@
+import styled from "styled-components/macro";
+
+const MaxWidthWrapper = styled.div`
+  position: relative;
+  /* max-width: min(100%, calc(1200px + 32px * 2)); */
+  padding-left: 128px;
+  padding-right: 128px;
+`;
+
+export default MaxWidthWrapper;
